40a3a94d9d96c4659a8eca9af13c4f201cc0d49d
Gagan Jeet Choudhary <126656547+Gagan690@users.noreply.github.com>
2025-08-10 13:35:04 +0530

    MaximumGap.py

---
+++ medium/MaximumGap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if not nums or len(nums) <= 1:
+            return 0
+
+        num = sorted(nums)
+        longest = num[1] - num[0]
+        for i in range(len(num)-1):
+            diff = num[i+1] - num[i]
+            if diff > longest:
+                longest = diff
+        return longest
